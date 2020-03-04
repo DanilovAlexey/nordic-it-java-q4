f439e8e254fc933b1806a1823740b32ad0f01436
DanilovAlexey <danilov_av90@list.ru>
2020-03-04 21:53:47 +0300

    отформатировал

--- 03/HomeWork/Kth/src/com/example/Kth.java
+++ 03/HomeWork/Kth/src/com/example/Kth.java
@@ -10,43 +10,43 @@ public class Kth {
 
 		var input = scanner.nextLong();
 		System.out.println(getResult(input));
-		
+
 		scanner.close();
-					
+
 	}
 
 	public static int getResult(long input) {
 		int result = 0;
-        var prevRangeLen = 0L;     
-        var prevRange = 0L;        
+		var prevRangeLen = 0L;
+		var prevRange = 0L;
 
-        var cnt = 1L;
-        var digits = 1L;
+		var cnt = 1L;
+		var digits = 1L;
 
-        while (prevRangeLen < input) {
-            if (prevRangeLen + (9 * cnt * digits) > input) {
-                break;
-            }
+		while (prevRangeLen < input) {
+			if (prevRangeLen + (9 * cnt * digits) > input) {
+				break;
+			}
 
-            prevRangeLen += 9 * cnt * digits;
-            prevRange += 9*cnt;
+			prevRangeLen += 9 * cnt * digits;
+			prevRange += 9 * cnt;
 
-            cnt = cnt * 10;
-            digits += 1;
-        }       
-        prevRangeLen -= 1;
-        
+			cnt = cnt * 10;
+			digits += 1;
+		}
+		prevRangeLen -= 1;
+
 		var curRangeLen = prevRangeLen + 1;
 		var curRange = prevRange + 1;
-			
+
 		var curNumber = (input - curRangeLen) / digits + curRange;
 		var curNumberRemain = (input - curRangeLen) % digits;
-		
+
 		for (var n = curNumberRemain; n < digits; n++) {
-			result = (int)(curNumber % 10);
+			result = (int) (curNumber % 10);
 			curNumber = curNumber / 10;
 		}
-		
-        return result;
-	}		
+
+		return result;
+	}
 }
