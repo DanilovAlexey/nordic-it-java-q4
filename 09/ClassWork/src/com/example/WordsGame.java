ac5e219c22d0d57c9da122b966693409dcb2f9a4
DanilovAlexey <danilov_av90@list.ru>
2020-03-22 22:50:08 +0300

    учел комментарии. добавил подсчет очков за буквы.

--- 09/ClassWork/src/com/example/WordsGame.java
+++ 09/ClassWork/src/com/example/WordsGame.java
@@ -1,18 +1,22 @@
 package com.example;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class WordsGame {
 
-	public static HashSet<Character> letters;
-
 	public static void main(String[] args) {
+
+		var scoreTable = new HashMap<String, Integer>();
+		scoreTable = getScoreTable();
+
+		var playersHistory = new HashMap<>();
 
 		var scanner = new Scanner(System.in);
 		var word = "";
 
-		letters = new HashSet<Character>();
+		var lettersSet = new HashSet<Character>();
 		var wordsHistory = new HashSet<String>();
 		var score = new int[2];
 
@@ -20,18 +24,21 @@ public class WordsGame {
 		word = scanner.nextLine();
 
 		for (int i = 0; i < word.length(); i++) {
-			letters.add(word.charAt(i));
+			lettersSet.add(word.charAt(i));
 		}
 
 		var wordNew = "";
 		var cnt = 0;
 		var cntPass = 0;
+		var player = "";
 
 		while (cntPass < 2) {
-			System.out.println("Ходит игрок " + ((cnt % 2 == 0) ? "A" : "B") + ":");
+			player = ((cnt % 2 == 0) ? "A" : "B");
+
+			System.out.println("Ходит игрок " + player + ":");
 			wordNew = scanner.nextLine();
 
-			if (!(validateWord(wordNew.toCharArray()))) {
+			if (!(validateWord(wordNew.toCharArray(), lettersSet))) {
 				System.out.println("Слово " + wordNew + " не подходит");
 				continue;
 			}
@@ -43,21 +50,24 @@ public class WordsGame {
 
 			if (!wordNew.isEmpty() || !wordNew.isBlank()) {
 				wordsHistory.add(wordNew);
-				if ((cnt % 2 == 0))
-					score[0] += 1;
-				else
-					score[1] += 1;
+
+				playersHistory.putIfAbsent(player, "");
+				var words = playersHistory.get(player);
+				words += wordNew;
+				playersHistory.put(player, words);
 
 				cntPass = 0;
 				cnt += 1;
-			}
-
-			if (wordNew.isEmpty() || wordNew.isBlank()) {
-				System.out.println("Игрок " + ((cnt % 2 == 0) ? "A" : "B") + " пропускает ход.");
+			} else {
+				System.out.println("Игрок " + player + " пропускает ход.");
 				cntPass += 1;
 				cnt += 1;
 			}
+
 		}
+
+		score[0] = finalCountdown(scoreTable, playersHistory.get("A").toString());
+		score[1] = finalCountdown(scoreTable, playersHistory.get("B").toString());
 
 		if (score[0] == score[1]) {
 			System.out.println("Ничья! Счет: " + score[0] + " : " + score[1]);
@@ -69,13 +79,60 @@ public class WordsGame {
 		scanner.close();
 	}
 
-	public static boolean validateWord(char[] arr) {
+	public static boolean validateWord(char[] arr, HashSet<Character> lettersSet) {
 		for (var letter : arr) {
-			if (!(letters.contains(letter)))
+			if (!(lettersSet.contains(letter)))
 				return false;
 		}
 
 		return true;
+	}
+
+	public static HashMap<String, Integer> getScoreTable() {
+		var map = new HashMap<String, Integer>();
+		map.put("А", 10);
+		map.put("Б", 3);
+		map.put("В", 5);
+		map.put("Г", 3);
+		map.put("Д", 5);
+		map.put("Е", 9);
+		map.put("Ж", 2);
+		map.put("З", 2);
+		map.put("И", 10);
+		map.put("Й", 4);
+		map.put("К", 6);
+		map.put("Л", 4);
+		map.put("М", 5);
+		map.put("Н", 8);
+		map.put("О", 10);
+		map.put("П", 6);
+		map.put("Р", 6);
+		map.put("С", 6);
+		map.put("Т", 5);
+		map.put("У", 3);
+		map.put("Ф", 10);
+		map.put("Х", 2);
+		map.put("Ц", 1);
+		map.put("Ч", 2);
+		map.put("Ш", 2);
+		map.put("Щ", 1);
+		map.put("Ъ", 1);
+		map.put("Ы", 2);
+		map.put("Ь", 2);
+		map.put("Э", 1);
+		map.put("Ю", 1);
+		map.put("Я", 4);
+
+		return map;
+	}
+
+	public static int finalCountdown(HashMap<String, Integer> scoreTable, String playerHistory) {
+		var result = 0;
+		for (var item : playerHistory.toCharArray()) {
+			result += scoreTable.get(String.valueOf(item).toUpperCase());
+		}
+
+		return result;
 	}
 
 }
