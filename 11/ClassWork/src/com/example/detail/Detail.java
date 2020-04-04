c5415b7fa234caea766b552274ec21c9e94bc11e
DanilovAlexey <danilov_av90@list.ru>
2020-04-05 01:07:37 +0300

    потоки

--- 11/ClassWork/src/com/example/detail/Detail.java
+++ 11/ClassWork/src/com/example/detail/Detail.java
@@ -1,9 +1,8 @@
 package com.example.detail;
 
-public class Detail implements Runnable {
+public class Detail {
 
 	private int detailNumber;
-	private int stageNumber;
 
 	public Detail(int detailNumber) {
 		super();
@@ -12,27 +11,6 @@ public class Detail implements Runnable {
 
 	public int getDetailNumber() {
 		return detailNumber;
-	}
-
-	public void setStageNumber(int stageNumber) {
-		this.stageNumber = stageNumber;
-	}
-
-	public int getStageNumber() {
-		return stageNumber;
-	}
-
-	@Override
-	public void run() {
-		System.out.println("Изделие №" + this.detailNumber + ". Стадия №" + this.stageNumber + ". Начало.");
-
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// e.printStackTrace();
-			Thread.currentThread().interrupt();
-		}
-		System.out.println("Изделие №" + this.detailNumber + ". Стадия №" + this.stageNumber + ". Конец.");
 	}
 
 }
